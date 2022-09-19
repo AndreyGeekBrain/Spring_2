464607ff250427142b1e8c3efedeee051e730a7f
”Andrey <Soda14@mail.ru>
2022-09-19 15:16:06 +0300

    DZ_Lesson_6

--- frontend/src/main/java/ru/gb/frontend/FrontendApplication.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.frontend;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-
-@SpringBootApplication
-@EnableEurekaClient
-public class FrontendApplication { }
